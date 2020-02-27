6fabf79f08829105e821027a1985081450b1a596
Rohit <rohit.kumar@mindfiresolutions.com>
2020-02-27 14:49:17 +0530

    grpc python client and server example

---
+++ client.py
@@ -0,0 +1,19 @@
+import grpc
+
+# import the generated classes
+import calculator_pb2
+import calculator_pb2_grpc
+
+# open a gRPC channel
+channel = grpc.insecure_channel('localhost:50051')
+
+# create a stub (client)
+stub = calculator_pb2_grpc.CalculatorStub(channel)
+
+# create a valid request message
+number = calculator_pb2.Number(value=20)
+
+# make the call
+response = stub.SquareRoot(number)
+
+print(response.value)
